d1d190fc63430052a4148cb5afe141456ebc7713
Sriram <sriram@petavue.com>
2024-11-24 07:49:48 +0530

    changes in dummy server to standardise routes and added docker compose for ease

---
+++ load_balancer/dummy_server/go.mod
@@ -0,0 +1,3 @@
+module github.com/sriramr98/dummy_server
+
+go 1.22.5
--- load_balancer/dummy_server/server.go
+++ load_balancer/dummy_server/server.go
@@ -4,19 +4,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Path struct {
+	message string
+	status  int
+}
+
 func main() {
+	serverId := os.Getenv("SERVER_ID")
+	paths := map[string]Path{
+		"/success": {
+			status:  http.StatusOK,
+			message: "success",
+		},
+		"/notfound": {
+			status:  http.StatusNotFound,
+			message: "not found",
+		},
+		"/internalerror": {
+			status:  http.StatusInternalServerError,
+			message: "internal server error",
+		},
+		"/health": {
+			status:  http.StatusFound,
+			message: "Status: OK",
+		},
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("========================")
+		fmt.Printf("Path Hit: %s", r.URL.Path)
 		for k, v := range r.Header {
 			fmt.Printf("Key: %s, Value: %s\n", k, v)
 		}
-		w.Write([]byte("Hello"))
-	})
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Health check route hit....")
-		w.Write([]byte("OK"))
+		w.Header().Set("server_id", serverId)
+		pathConfig, ok := paths[r.URL.Path]
+		if !ok {
+			w.Header().Set("hit", "invalid_path")
+			w.WriteHeader(http.StatusNotImplemented)
+			w.Write([]byte("invald path"))
+		}
+
+		w.Header().Set("hit", r.URL.Path)
+		w.WriteHeader(pathConfig.status)
+		w.Write([]byte(pathConfig.message))
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
