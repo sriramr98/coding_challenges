bace4ac1ed918af7953005aa6e53f5a8f1186995
Sriram <sriram@petavue.com>
2024-11-27 15:14:09 +0530

    added https support and graceful shutdown

--- load_balancer/core/config.go
+++ load_balancer/core/config.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -15,20 +16,41 @@ type HealthCheckConfig struct {
 	Timeout           int    `yaml:"timeout"`
 }
 
+type TLSConfig struct {
+	Certificate string `yaml:"cert"`
+	Key         string `yaml:"key"`
+}
+
+func (t TLSConfig) IsValidTLS() bool {
+	return len(t.Certificate) > 0 && len(t.Key) > 0
+}
+
 type Config struct {
 	HttpVersion       string            `yaml:"http_version"`
 	Protocol          string            `yaml:"protocol"`
 	BalanceStrategy   string            `yaml:"balance_strategy"`
+	Tls               TLSConfig         `yaml:"tls"`
 	HealthCheckConfig HealthCheckConfig `yaml:"health_check"`
 	Port              int               `yaml:"port"`
 }
 
 func GetConfig() Config {
-	if len(os.Args) < 2 {
+	filePath := os.Getenv("CONFIG_PATH")
+	fmt.Printf("Evn file path: %s\n", filePath)
+
+	if filePath == "" {
+		if len(os.Args) < 2 {
+			log.Fatal("Config file is required")
+		}
+
+		filePath = os.Args[1]
+	}
+
+	if filePath == "" {
 		log.Fatal("Config file is required")
 	}
 
-	filePath := os.Args[1]
+	fmt.Printf("File Path: %s\n", filePath)
 	configContent, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
--- load_balancer/lb.go
+++ load_balancer/lb.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/sriramr98/load_balancer/core"
 	"github.com/sriramr98/load_balancer/strategies"
@@ -16,74 +22,147 @@ import (
 var ErrNoIPFound = errors.New("no IP Found in request")
 
 type LoadBalancer struct {
-	strategy strategies.BalancingStrategy
-	config   core.Config
+	requestsWg            *sync.WaitGroup
+	strategy              strategies.BalancingStrategy
+	config                core.Config
+	shouldProcessRequests int32 // 0 if should process, 1 if should not process. Used for graceful shutdown
 }
 
 func NewLoadBalancer(config core.Config, strategy strategies.BalancingStrategy) LoadBalancer {
 	return LoadBalancer{
-		config:   config,
-		strategy: strategy,
+		config:                config,
+		strategy:              strategy,
+		requestsWg:            &sync.WaitGroup{},
+		shouldProcessRequests: 0,
 	}
 }
 
-func (lb LoadBalancer) Start() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Path: %s\n", r.URL.Path)
-		headers := []core.Header{}
+func (lb *LoadBalancer) Start() {
+	server := &http.Server{
+		Handler: lb,
+	}
 
-		for key, values := range r.Header {
-			headers = append(headers, core.Header{
-				Key:    key,
-				Values: values,
-			})
-		}
-
-		httpReq := core.HttpReq{
-			Path:    r.URL.Path,
-			Method:  r.Method,
-			Body:    r.Body,
-			Headers: headers,
-		}
-
-		ip, err := extractIP(r)
-		if err != nil {
-			// We don't reject the request here because the RoutingStrategy will reject if Ip is required, else let it go through
-			log.Println(err.Error())
-		}
-
-		server, err := lb.strategy.Next(strategies.LBStrategyParams{
-			IpAddress: ip,
-		})
-		log.Printf("Reaching Server %s", server.GetID())
-		if err != nil {
-			log.Printf("Error: %s\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("No valid server found"))
-			return
-		}
-
-		res, err := server.PerformHTTPRequest(httpReq)
-		if err != nil {
-			log.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Unable to reach server"))
-			return
-		}
-
-		for _, header := range res.Headers {
-			for _, value := range header.Values {
-				w.Header().Set(header.Key, value)
+	if lb.config.Tls.IsValidTLS() {
+		log.Println("Started HTTPS server on port 443")
+		go func() {
+			// http.HandleFunc("/", lb.HandleRequest)
+			ln, err := net.Listen("tcp", ":443")
+			if err != nil {
+				log.Fatal(err)
 			}
-		}
-		w.WriteHeader(res.StatusCode)
-		io.Copy(w, res.Body)
-	})
 
-	log.Printf("Listening on port %d\n", lb.config.Port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", lb.config.Port), nil); err != nil {
+			if err := server.ServeTLS(ln, lb.config.Tls.Certificate, lb.config.Tls.Key); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("HTTPS serve completed..")
+		}()
+	}
+
+	go func() {
+		log.Println("Start HTTP server on port 80")
+		ln, err := net.Listen("tcp", ":80")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err = server.Serve(ln); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("HTTP serve completed")
+	}()
+
+	lb.listenAndInitiateGracefulShutdown(server)
+}
+
+func (lb LoadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lb.requestsWg.Add(1)
+	defer lb.requestsWg.Done()
+
+	if atomic.LoadInt32(&lb.shouldProcessRequests) == 1 {
+		log.Printf("Request for path %s rejected since LB is in shutdown mode", r.URL.Path)
+		http.Error(w, "Unable to process request", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("shouldProcessRequests %d\n", atomic.LoadInt32(&lb.shouldProcessRequests))
+
+	log.Printf("Path: %s\n", r.URL.Path)
+	headers := []core.Header{}
+
+	for key, values := range r.Header {
+		headers = append(headers, core.Header{
+			Key:    key,
+			Values: values,
+		})
+	}
+
+	httpReq := core.HttpReq{
+		Path:    r.URL.Path,
+		Method:  r.Method,
+		Body:    r.Body,
+		Headers: headers,
+	}
+
+	ip, err := extractIP(r)
+	if err != nil {
+		// We don't reject the request here because the RoutingStrategy will reject if Ip is required, else let it go through
+		log.Println(err.Error())
+	}
+
+	server, err := lb.strategy.Next(strategies.LBStrategyParams{
+		IpAddress: ip,
+	})
+	log.Printf("Reaching Server %s", server.GetID())
+	if err != nil {
+		log.Printf("Error: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("No valid server found"))
+		return
+	}
+
+	res, err := server.PerformHTTPRequest(httpReq)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unable to reach server"))
+		return
+	}
+
+	for _, header := range res.Headers {
+		for _, value := range header.Values {
+			w.Header().Set(header.Key, value)
+		}
+	}
+	w.WriteHeader(res.StatusCode)
+	io.Copy(w, res.Body)
+}
+
+func (lb *LoadBalancer) listenAndInitiateGracefulShutdown(server *http.Server) {
+	// Shutdown when we receive Ctrl+c (interrupt)
+	c := make(chan os.Signal, 1)
+
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+	fmt.Println("Got os.Interrupt signal")
+
+	// Mark shouldProcessRequests as 1 so that any requests that come in while we wait for existing to finish are rejected by handlers
+	atomic.StoreInt32(&lb.shouldProcessRequests, 1)
+
+	// Waits for all processing requests to complete before shutting down the server
+	lb.requestsWg.Wait()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("Shutdown error")
 		log.Fatal(err)
 	}
+
+	fmt.Println("All servers shut down successfully..")
 }
 
 func extractIP(r *http.Request) (string, error) {
